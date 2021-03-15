a7206076be9dd385198ef97536abbee4d9ef20e3
Elite Ewok <66619920+EliteEwok@users.noreply.github.com>
2021-03-15 15:29:50 +0000

    Update OriginClearCache.bat
    Added check to see if Origin is running.
    Added Messages for Fail and Success.

--- OriginClearCache.bat
+++ OriginClearCache.bat
@@ -4,8 +4,19 @@ SET "sourcedir=%programdata%\Origin"
 SET "keepdir=LocalContent"
 SET "sourcedirect=%localappdata%\Origin"
 
-FOR /d %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepdir%" RD /S /Q "%%a"
+tasklist /FI "IMAGENAME eq Origin.exe" 2>NUL | find /I /N "Origin.exe">NUL
+if "%ERRORLEVEL%"=="0" GOTO originrunning
+
+FOR /d %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepdir%" RD /S /Q "%%a
 
 FOR /d %%a IN ("%sourcedirect%\*") DO IF /i NOT "%%~nxa"=="%keepdir%" RD /S /Q "%%a"
 
+msg %username% Done!
+GOTO end
+
+:originrunning
+msg %username% Origin is running. No files removed. Close Origin before running this script.
+GOTO end
+
+:end
 EXIT
