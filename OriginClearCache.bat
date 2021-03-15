e374c68089179ba5a15dbec974a66cafc5c779d4
Elite Ewok <66619920+EliteEwok@users.noreply.github.com>
2021-03-15 14:47:30 +0000

    Create OriginClearCache.bat

---
+++ OriginClearCache.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SETLOCAL
+SET "sourcedir=%programdata%\Origin"
+SET "keepdir=LocalContent"
+SET "sourcedirect=%localappdata%\Origin"
+
+FOR /d %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepdir%" RD /S /Q "%%a"
+
+FOR /d %%a IN ("%sourcedirect%\*") DO IF /i NOT "%%~nxa"=="%keepdir%" RD /S /Q "%%a"
+
+GOTO :EOF
